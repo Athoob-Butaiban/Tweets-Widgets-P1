ba5ac127d69cf637c41274cfc205409f912a04ec
athouba99 <athooba99@gmail.com>
2022-11-17 03:59:56 +0300

    mostly done with the first card, but image & share line need fixing

--- lib/twiteer.dart
+++ lib/twiteer.dart
@@ -4,27 +4,68 @@ class Twitter extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
+          centerTitle: true, // centring the text
           elevation: 0, // remove the shadow under the appBar
-          title: Text("Twitter"),
+          title: Text("Twitter"), // the tet in the appbar
         ),
-        body: Center(
-            child: Column(
+        body: Column(
           children: [
-            Container(
-              child: Text(
-                "Elon Musk",
-                style: TextStyle(fontWeight: FontWeight.bold),
+            Padding(
+              padding: const EdgeInsets.all(4),
+              child: Card(
+                color: Color.fromARGB(
+                    243, 255, 255, 255), // the way of adding a color
+                child: Row(children: [
+                  Column(
+                    children: [
+                      // Image.asset("assesta/images/elon.jpg"),
+                      // unable to load asset
+                      Row(
+                        children: [
+                          Container(
+                            child: Text(
+                              "Elon Musk",
+                              style: TextStyle(fontWeight: FontWeight.bold),
+                            ),
+                          ),
+                        ],
+                      ),
+                      Row(
+                        children: [
+                          Text("@elonmusk"),
+                        ],
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.only(left: 26, top: 25),
+                        child: Text("Comedy is now legal on Twitter"),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.only(
+                          left: 10,
+                          top: 30,
+                        ),
+                        child: Row(
+                          children: [
+                            // 2 text widget are need in this case BC of the different color they have
+                            Text("5:16 PM . 2022-10-28 .",
+                                style: TextStyle(color: Colors.grey)),
+                            Text("Twitter for iPhone",
+                                style: TextStyle(color: Colors.blueAccent)),
+                          ],
+                        ),
+                        // child: Row(
+                        //   children: [
+                        //     Row(
+                        //       children: [Icon(Icons.favorite), Text("16")],
+                        //     )
+                        //   ],
+                      )
+                    ],
+                  ),
+                ]),
               ),
             ),
-            Padding(
-              padding: const EdgeInsets.all(10),
-              child: Text("@elonmusk"),
-            ),
-            Padding(
-              padding: const EdgeInsets.all(10),
-              child: Text("Comedy is now legal on Twitter"),
-            ),
           ],
-        )));
+        ));
   }
 }
